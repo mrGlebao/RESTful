d2a3b45190eb7172354edbd13d78bd691875a4af
Glebus <zaborovskij.gleb@gmail.com>
2018-04-11 00:30:57 +0300

    * Added account resource

---
+++ src/main/java/com/revolut/test/resources/AccountResource.java
@@ -0,0 +1,45 @@
+package com.revolut.test.resources;
+
+import com.codahale.metrics.annotation.Timed;
+import com.revolut.test.db.AccountDAO;
+import com.revolut.test.dto.AccountDTO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/accounts")
+@Produces(MediaType.APPLICATION_JSON)
+public class AccountResource {
+
+    private final AccountDAO dao;
+
+    public AccountResource(AccountDAO dao) {
+        this.dao = dao;
+    }
+
+    @GET
+    @Path("/{user}/view")
+    @Timed
+    public Response getUser(@PathParam("user") int user) {
+        return Response.ok(new AccountDTO(user, dao.findAmountById(user))).build();
+    }
+
+    @PUT
+    @Path("/{user}/add")
+    @Timed
+    public Response put(@PathParam("user") int user, @QueryParam("amount") int amount) {
+        dao.insert(user, amount);
+        return Response.ok(new AccountDTO(user, dao.findAmountById(user))).build();
+    }
+
+    @POST
+    @Path("/{user}/update")
+    @Timed
+    public Response update(@PathParam("user") int user, @QueryParam("amount") int amount) {
+        dao.update(user, amount);
+        return Response.ok(new AccountDTO(user, dao.findAmountById(user))).build();
+    }
+
+}
