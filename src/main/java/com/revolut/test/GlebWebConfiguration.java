ee6e50a70b1734bd43eedf7740ad7d881fbc2ddc
Glebus <zaborovskij.gleb@gmail.com>
2018-04-11 00:26:05 +0300

    Added java configuration options to support h2

--- src/main/java/com/revolut/test/GlebWebConfiguration.java
+++ src/main/java/com/revolut/test/GlebWebConfiguration.java
@@ -2,9 +2,26 @@ package com.revolut.test;
 
 import io.dropwizard.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.*;
+
+import javax.validation.Valid;
 import javax.validation.constraints.*;
 
 public class GlebWebConfiguration extends Configuration {
-    // TODO: implement service configuration
+
+    @Valid
+    @NotNull
+    @JsonProperty
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory factory) {
+        this.database = factory;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
 }
